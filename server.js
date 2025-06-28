c55b068e11869f92e3731c89b279485db0ec9ba2
nnquynh0804 <101651551+nnquynh0804@users.noreply.github.com>
2025-06-29 02:05:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
